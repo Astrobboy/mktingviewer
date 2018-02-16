9b1fb7d2e1e52bf3719f4f53ed9437f8ce6a3351
astrosalinas <astrosalinas35@gmail.com>
2018-02-16 15:11:23 -0300

    mod pb 6

--- static/js/video.js
+++ static/js/video.js
@@ -62,6 +62,7 @@ window.onbeforeunload = function (event) {
 
 
 video.addEventListener("ended", function() {
+	localStorage.setItem(clave_tiempo_actual, '0');
 	nom_videos = JSON.parse((localStorage.getItem(clave_lista_videos)));	
 	if(Math.trunc(localStorage.getItem(clave_tiempo_actual)) != Math.trunc(localStorage.getItem(clave_duracion_video))){
 		location.replace("http://192.168.100.21/play");
