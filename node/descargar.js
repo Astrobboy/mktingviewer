504fac479f4a7a6fcdbf6cd878546bf7c84cb432
astrosalinas <astrosalinas35@gmail.com>
2018-03-22 13:16:07 -0300

    modificacion de script que actualiza db

---
+++ node/descargar.js
@@ -0,0 +1,47 @@
+const jsonlint = require("jsonlint");
+const axios = require("axios");
+const shell = require('shelljs');
+
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url_db = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'atacado_producto';
+
+const result = '';
+
+
+
+MongoClient.connect(url_db, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    const collection = db.collection('producto');
+    //busco todos los datos de db
+    collection.find({}).toArray(function (err, result) {
+        if (err) throw err;
+        result = result;
+        var codigo = Object.keys(result);
+
+        //comparo los valores de result pag y result db codigo.length
+        for (var i = 0; i < codigo.length ; i++) {
+
+            //guarda en valor el objeto de la clave en ese momento
+            var valor = Object.keys(result[codigo[i]]);
+            //obtengo en otro objeto el valor del objeto anterior
+            var values_object = result[codigo[i]];
+            //console.log(values_object);
+            //compruebo el array de db con el de pag
+            for (var y = 0; y < values_object[valor[4]].length; y++) {
+                // cambiar a esta ruta
+                // /home/astro/mktingviewer/static/img/productos/
+                console.log(values_object[valor[4]][y]);
+                shell.exec(`../checkfile.sh ${values_object[valor[4]][y]}`)
+               // console.log('termine');
+            }
+        }
+        client.close();
+    })
+});
---
+++ node/prueba.js
@@ -0,0 +1,159 @@
+const jsonlint = require("jsonlint");
+const axios = require("axios");
+const shell = require('shelljs');
+
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url_db = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'atacado_producto';
+
+//const que va a probar si el array se modifico o no
+const valor_prueba = true;
+const result = '';
+
+const URL = "http://www2.atacadogames.com/app/produtos.json";
+
+
+MongoClient.connect(url_db, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    const collection = db.collection('producto');
+   //busco todos los datos de db
+    collection.find({}).toArray(function (err, result) {
+        if (err) throw err;
+        result = result;
+        //si todo okey hago la peticion del json
+        axios.get(URL).then(response => {
+            var json = jsonlint.parse(JSON.stringify(response.data));
+            if (json) {
+                //obtengo codigo de los objeto 
+                var codigo = Object.keys(json);
+                
+                //comparo los valores de json pag y json db codigo.length
+                for (var i = 0; i < 2 ; i++) {
+
+                    //guarda en valor el objeto de la clave en ese momento
+                    var valor = Object.keys(json[codigo[i]]);
+                    //obtengo en otro objeto el valor del objeto anterior
+                    var values_object = json[codigo[i]];
+                    //compruebo el array de db con el de pag
+                    if (values_object.link_images.length != result[i].link_images.length ){
+                        valor_prueba = false;
+                        console.log('aqui');
+                    }
+                    
+                    if (values_object.title == result[i].title &&
+                        values_object.description == result[i].description &&
+                        valor_prueba){
+                            console.log('no hubo cambios');
+                            client.close();
+                            for (var y = 0; y < values_object[valor[2]].length; y++) {
+                                // cambiar a esta ruta
+                                // /home/astro/mktingviewer/static/img/productos/
+                                shell.exec(`../checkfile.sh ${values_object[valor[2]][y]}`)
+                                console.log('termine');
+                            }
+                    }
+                    
+                }
+                console.log(valor_prueba);
+               
+                // si son distintos va a borrar los datos e insertar de nuevo
+                if (valor_prueba != true ){
+                    //console.log(result);
+                    collection.remove({}).then(() => {
+                        //for par agrupar los datos
+                        for (var i = 0; i < 2; i++) {
+                            //guarda en valor el objeto de la clave en ese momento
+                            var valor = Object.keys(json[codigo[i]]);
+                            //obtengo en otro objeto el valor del objeto anterior
+                            var values_object = json[codigo[i]];
+                            // Insert some producto
+                            console.log('codigo:', codigo[i]);
+                            console.log('title:', values_object[valor[0]]);
+                            console.log('description:', values_object[valor[1]]);
+                            console.log('link_images:', values_object[valor[2]]);
+                            console.log('price:', values_object[valor[3]]);
+                            console.log('stock:', values_object[valor[4]]);
+                            collection.insertMany([
+                                {
+                                    codigo: codigo[i],
+                                    title: values_object[valor[0]],
+                                    description: values_object[valor[1]],
+                                    link_images: values_object[valor[2]],
+                                    price: values_object[valor[3]],
+                                    stock: values_object[valor[4]]
+                                }
+ 
+                            ]);
+                            console.log('link_images: ',values_object[valor[2]]);
+                            for (var y = 0; y < values_object[valor[2]].length; y++){
+                                shell.exec(`../checkfile.sh ${values_object[valor[2]][y]}`);
+                            }                            
+                        }
+                        console.log("Sali!");
+                        client.close();
+                    }).catch(e => console.log(e));
+                }  
+            }
+        }).catch(e => console.log(e));
+    });
+    
+});
+
+
+
+
+/*
+// Use connect method to connect to the server
+MongoClient.connect(url_db, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    axios.get(URL).then(response => {
+
+        var json = jsonlint.parse(JSON.stringify(response.data));
+        if (json) {
+            //obtengo codigo de los objeto 
+            var codigo = Object.keys(json);
+            const db = client.db(dbName);
+            // Get the producto collection
+            const collection = db.collection('producto');
+            //TODO -> restructurar la logica para que no apague de balde... Hay cambios? Actualizar directo en la base de dados!
+           collection.remove({}).then(() => {
+                //for par agrupar los datos
+                for (var i = 0; i < codigo.length; i++) {
+
+                    //guarda en valor el objeto de la clave en ese momento
+                    var valor = Object.keys(json[codigo[i]]);
+                    //obtengo en otro objeto el valor del objeto anterior
+                    var values_object = json[codigo[i]];
+                    // Get the producto collection
+                    const collection = db.collection('producto');
+                    // Insert some producto
+                    collection.insertMany([
+                        {
+                            codigo: codigo[i],
+                            title: values_object[valor[0]],
+                            description: values_object[valor[1]],
+                            link_images: values_object[valor[2]],
+                            price: values_object[valor[3]],
+                            stock: values_object[valor[4]]
+                        }
+                    ]);
+                    //TODO -> for x in link_images: values_object[valor[2]].keys() y 
+                    //ejecutar syscall al script ../checkfile.sh [link]. Descargar las imagenes
+                    //OJO! quitar la linea de codigo en app.py que ejecuta este comando
+                    //Ver https://stackoverflow.com/questions/44647778/how-to-run-shell-script-file-using-nodejs
+                }
+                client.close();
+                console.log("Sali!");
+            });
+        }
+    }).catch(e => console.log(e));
+});
+*/
