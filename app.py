8b9c8852cac98ab1cca2e9cd07678885dcd5565f
astrosalinas <astrosalinas35@gmail.com>
2018-07-18 14:30:49 -0400

    test

--- app.py
+++ app.py
@@ -338,12 +338,15 @@ def handle_connection():
             if (data_ip['creacion'] == data_video['creacion']):
                 # si es solo almaceno tiempo y cont
                 if (len(data_video['lista']) <=  request.json["cont"]):
+                    print "++++++++"
                     print request.json["cont"]
                     data_ip['cont'] = 0 
                 else:                  
                     #print "estoy aqui y no deberia >>> %d <= %d" %(len(data_video['lista']),request.json["cont"])
                     data_ip['cont'] = request.json["cont"]
             else:
+                print "=============="
+                print request.json["cont"]
                 data_ip['creacion'] = data_video['creacion']
                 data_ip['cont'] = 0
             data_ip['tiempo'] = request.json["tiempo"]
